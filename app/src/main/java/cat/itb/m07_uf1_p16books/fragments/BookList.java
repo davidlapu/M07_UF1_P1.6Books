c8ec55f9e608a127cb1ac9f29217a63cd11239d4
David Lama Pulido <david.lama.7e3@itb.cat>
2020-11-27 18:12:41 +0100

    a

---
+++ app/src/main/java/cat/itb/m07_uf1_p16books/fragments/BookList.java
@@ -0,0 +1,16 @@
+package cat.itb.m07_uf1_p16books.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cat.itb.m07_uf1_p16books.R;
+
+public class BookList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.book_list);
+    }
+}
