dc6577874c739b6ee1760a97d7a879bb7d08e80e
David Lama Pulido <david.lama.7e3@itb.cat>
2020-12-04 18:14:36 +0100

    a

---
+++ app/src/main/java/cat/itb/m07_uf1_p16books/fragments/BookListFragment.java
@@ -0,0 +1,7 @@
+package cat.itb.m07_uf1_p16books.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class BookListFragment extends Fragment {
+
+}
--- app/src/main/java/cat/itb/m07_uf1_p16books/recycler_view/BookAdapter.java
+++ app/src/main/java/cat/itb/m07_uf1_p16books/recycler_view/BookAdapter.java
@@ -1,4 +1,64 @@
 package cat.itb.m07_uf1_p16books.recycler_view;
 
-public class BookAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import cat.itb.m07_uf1_p16books.R;
+import cat.itb.m07_uf1_p16books.models.Book;
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder>{
+    private List<Book> bookList;
+
+    public BookAdapter(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.book_list_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Book b = bookList.get(position);
+
+        holder.getTextViewAuthor().setText(b.getAuthor());
+        holder.getTextViewStatus().setText(b.getStatus());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textViewAuthor, textViewStatus;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewAuthor = itemView.findViewById(R.id.textViewTitle);
+            textViewStatus = itemView.findViewById(R.id.textViewStatus);
+        }
+
+        public TextView getTextViewAuthor() {
+            return textViewAuthor;
+        }
+
+        public TextView getTextViewStatus() {
+            return textViewStatus;
+        }
+    }
 }
